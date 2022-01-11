71c531365c5451b750a6717bc2b26b0a18719360
CodeSevenic <sbu.audio.production@gmail.com>
2022-01-11 17:16:00 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// bring routes
+const blogRoutes = require('./routes/blog-routes');
+
+// app
+
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log('Database Connected'));
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+if (process.env.NODE_ENV == 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+// routes middleware
+app.use('/api', blogRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
